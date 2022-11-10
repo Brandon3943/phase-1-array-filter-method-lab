fc380a07861a29faa81d0352dcc1704eb694de66
root <root@Ty-Laptop.localdomain>
2022-11-10 18:58:35 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+
+
+function findMatching(driver, name) {
+  return driver.filter(driver => driver.toLowerCase() === name.toLowerCase());
+}
+
+
+function fuzzyMatch(driver, name) {
+  return driver.filter(driver => driver.charAt(0) === name.charAt(0));
+}
+
+const drivers = [
+  {
+    name: 'Bobby',
+    hometown: 'Pittsburgh' },
+  {
+    name: 'Sammy',
+    hometown: 'New York' } ,
+  {
+    name: 'Sally',
+    hometown: 'Cleveland' },
+  {
+    name: 'Annette',
+    hometown: 'Los Angeles' },
+  {
+    name: 'Bobby',
+    hometown: 'Tampa Bay' }
+];
+
+function matchName(driver, name) {
+  return driver.filter(driver => name === driver.name);
+}
+
+console.log(matchName(drivers, 'Bobby'));
